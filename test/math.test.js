3dc0435bd35fb137e528a1167a0554a539710a13
factory-smoke <factory@rubik.local>
2026-09-23 03:08:36 +0800

    feat(math): add mod with floored semantics (factory M1 smoke)

--- test/math.test.js
+++ test/math.test.js
@@ -1,5 +1,5 @@
 const assert = require("node:assert");
-const { add, sub, mul, div, pow } = require("../src/math");
+const { add, sub, mul, div, pow, mod } = require("../src/math");
 
 assert.strictEqual(add(2, 3), 5);
 assert.strictEqual(add(-1, 1), 0);
@@ -13,5 +13,9 @@ assert.throws(() => div(1, 0), /division by zero/);
 assert.strictEqual(pow(2, 10), 1024);
 assert.strictEqual(pow(5, 0), 1);
 assert.throws(() => pow(2, -1), /non-negative integer/);
+
+assert.strictEqual(mod(7, 3), 1);
+assert.strictEqual(mod(-1, 3), 2);
+assert.throws(() => mod(1, 0), /modulo by zero/);
 
 console.log("math.test.js: all assertions passed");
